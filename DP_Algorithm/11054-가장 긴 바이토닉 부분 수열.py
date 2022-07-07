473854fc9d02d697ef9a580649d69e9329e01310
tkfdkskarl56 <tkfdkskarl56@naver,com>
2022-07-07 21:39:54 +0900

    DP_Algorithm LIS

--- DP_Algorithm/11054-가장 긴 바이토닉 부분 수열.py
+++ DP_Algorithm/11054-가장 긴 바이토닉 부분 수열.py
@@ -1,23 +1,28 @@
 n = int(input())
 
 numL = list(map(int, input().split()))
-maxNum = max(numL)
-dp = [0] * n
+
+dp = [[ 0 for _ in range(2) ] for _ in range(n)]
+
 for i in range(n):
     if i == 0:
-        dp[0] = 1
+        dp[0][0], dp[n-1][1] = 1, 1
     else:
-        for j in range(0, i):
-            max_dp=0
-            min_dp=1001
-            if numL[i] == maxNum:
-                if min_dp > dp[j] and numL[j] < numL[i]:
-                    min_dp = dp[j]
-                dp[i] = min_dp+1
-            else:
-                if max_dp < dp[j] and numL[j] < numL[i]:
-                    max_dp = dp[j]
-                dp[i] = max_dp+1
+        max_dp=0
+        for j in range(i):
+            if max_dp < dp[j][0] and numL[j] < numL[i]:
+                max_dp = dp[j][0]
+            dp[i][0] = max_dp + 1
+        max_dp=0
+        for j in range(n-i,n):
+            if max_dp < dp[j][1] and numL[j] < numL[n-i-1]:
+                max_dp = dp[j][1]
+            dp[n-i-1][1] = max_dp +1
+max = 0
+for i in range(n):
+    if max < dp[i][0]+dp[i][1]:
+        max = dp[i][0]+dp[i][1]
+        print(max, dp[i][0]+dp[i][1])
 
-print(dp)
+
 
