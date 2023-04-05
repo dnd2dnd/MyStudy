aefcdbf922a66248e0e9d1be218e632f87c9c650
tkfdkskarl56 <tkfdkskarl56@naver.com>
2023-04-05 23:25:45 +0900

    백준 장군

--- 백준/16509. 장군.py
+++ 백준/16509. 장군.py
@@ -1,48 +1,50 @@
+from collections import deque
 r1, c1 = map(int, input().split())
 r2, c2 = map(int, input().split())
 
 board = [[0]*9 for i in range(10)]
-visited = board
+visited = [[0]*9 for i in range(10)]
 
-dx = [-2,2,3,3,2,-2,-3,-3]
-dy = [-3,-3,-2,2,3,3,2,-2]
-px = [-1,0,1,2,1,2,1,0,-1,-2,-1,-2]
-py = [-2,-1,-2,-1,0,1,2,1,2,1,0,-1]
+dy = [-2,2,3,3,2,-2,-3,-3]
+dx = [-3,-3,-2,2,3,3,2,-2]
+py = [-1,0,1,2,1,2,1,0,-1,-2,-1,-2]
+px = [-2,-1,-2,-1,0,1,2,1,2,1,0,-1]
 
-def dfs(x,y):
-    count = 0
-    co=0
-    if visited[x][y]==0:
-        count+=1
-        visited[x][y]=1
 
-    for i in range(8):
-        xx=x+dx[i]
-        yy=y+dy[i]
+def bfs(x,y):
+    queue = deque([])
+    queue.append((x,y))
+    
+    while queue:
+        count = 0
+        x, y = queue.popleft()
         
-        if xx<0 or yy<0 or xx>=10 or yy>=9:
-            continue
+        if x==r2 and y==c2:
+            print(board[x][y])
+            break
 
-        if visited[xx][yy]==1:
-            co+=1
-        if visited[xx][yy]==0:
-            k = i+i//2
-            check=0
-            # for k in range(2):
-            #     qx=x+px[k]
-            #     qy=y+py[k]
-            #     if qx!=r2 and qy!=c2:
-            #         check+=1
-            if check==2:
-                print("@",xx,yy)
-                if xx==r2 and yy==c2:
-                    return count
-                count += dfs(xx,yy)
-
-        if co==8:
-            return -1
-    return count
-print(dfs(r1,c1))
+        for i in range(8):
+            nx = x+ dx[i]
+            ny = y+ dy[i]
             
-                    
+            if nx < 0 or ny < 0 or nx>=10 or ny>=9:
+                continue
+            # print(nx,ny)
+            if visited[nx][ny]==1:
+                continue
+            if visited[nx][ny]==0:
+                key=i+(i//2)
+                check = 0
+                # if nx==2 and ny==5:
+                for k in range(key, key+2):
+                    qx, qy = x+px[k],y+py[k]
+                    if qx==r2 and qy==c2:
+                        check+=1
+                if check==0:        
+                    # print(nx, ny)
+                    visited[nx][ny]=1
+                    board[nx][ny]=board[x][y]+1
+                    queue.append((nx,ny))
+
+bfs(r1,c1)
 
