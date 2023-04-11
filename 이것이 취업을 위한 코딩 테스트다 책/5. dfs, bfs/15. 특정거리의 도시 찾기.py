14022a3810a01ea4317c6c5e030b23d83b09da0e
tkfdkskarl56 <tkfdkskarl56@naver.com>
2023-04-12 00:38:34 +0900

    특정거리의 도시 찾기

--- 이것이 취업을 위한 코딩 테스트다 책/5. dfs, bfs/15. 특정거리의 도시 찾기.py
+++ 이것이 취업을 위한 코딩 테스트다 책/5. dfs, bfs/15. 특정거리의 도시 찾기.py
@@ -1,7 +1,31 @@
-n, m, k, x = map(int, input().split())
-li = [[]]
-for i in range(m):
-    li.append(list(map(int, input().split())))
+from collections import deque
 
-def sol(graph, start):
-    
+n, m, k, x = map(int, input().split())
+
+li = [[] for _ in range(n+1)]
+for i in range(m):
+    a, b = map(int, input().split())
+    li[a].append(b)
+
+distance = [-1] * (n+1)
+distance[x] = 0
+val=[]
+
+def sol(graph, s):
+    queue = deque([s])
+
+    while queue:
+        now = queue.popleft()
+        for node in graph[now]:
+            if distance[node] == -1:
+                distance[node] = distance[now] + 1
+                queue.append(node)
+
+check = False
+for i in range(1, n+1):
+    if distance[i] == k:
+        print(i)
+        check = True
+
+if check == False:
+    print(-1)
