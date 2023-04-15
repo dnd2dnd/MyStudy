6d75ea5bcf8cc0b47f84cc7d1d4352b342d09134
tkfdkskarl56 <tkfdkskarl56@naver.com>
2023-04-15 13:00:00 +0900

    2023-04-16 TIL

--- Python/라이브러리.py
+++ Python/라이브러리.py
@@ -6,3 +6,8 @@ print(list(permutations(data, 3)))
 print(list(combinations(data, 3)))
 print(list(product(data, repeat=3)))
 print(list(combinations_with_replacement(data, 3)))
+
+from collections import Counter
+
+counter = Counter(data)
+print(counter)
