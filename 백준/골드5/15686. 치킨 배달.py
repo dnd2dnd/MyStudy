3e0982fbe8708c8071f49d3977a2dc441e1cd555
tkfdkskarl56 <tkfdkskarl56@naver.com>
2023-04-18 23:52:42 +0900

    커밋

--- 백준/골드5/15686. 치킨 배달.py
+++ 백준/골드5/15686. 치킨 배달.py
@@ -11,10 +11,11 @@ for x in range(n):
         elif board[x][y]==2:
             chicken.append((x,y))
 sumL = []
-for chi in chicken:
-    sum = 0
-    for ho in home:
-        sum+=(abs(chi[0]-ho[0]) + abs(chi[1]-ho[1]))
-    sumL.append(sum)
 
+for chi in chicken:    
+    sum = []
+    for ho in home:    
+        sum.append(abs(chi[0]-ho[0]) + abs(chi[1]-ho[1]))
+    sumL.append(sum)
+sumL.sort()
 print(sumL)
