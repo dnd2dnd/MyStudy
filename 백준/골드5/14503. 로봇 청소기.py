e39d500cebce5284e91f1917c8e2ef1b44320f0a
tkfdkskarl56 <tkfdkskarl56@naver.com>
2023-04-20 01:23:10 +0900

    로봇 청소기

--- 백준/골드5/14503. 로봇 청소기.py
+++ 백준/골드5/14503. 로봇 청소기.py
@@ -1,8 +1,8 @@
 n, m = map(int, input().split())
 x, y, d = map(int, input().split())
 board = []
-dx = [0, 1, 0, -1]
-dy = [-1, 0, 1, 0]
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 
 for i in range(n):
     board.append(list(map(int, input().split())))
@@ -11,33 +11,28 @@ def dfs(x, y, d):
     global cnt
     if board[x][y]==0:       
         cnt+=1
-        board[x][y]=2 
-    check = 0
-    for i in range(1,5):
-        if d-i<0:
-            dd=d-i+4
-        else:
-            dd= d-i
-        xx = x+dx[dd]
-        yy = y+dy[dd]
-        if xx<0 or yy<0 or xx>=n or yy>=m or board[xx][yy]==1:
+        board[x][y]=2
+    check=0
+    for i in range(4):
+        dd=(d+3-i)%4
+        xx, yy = x+dx[dd], y+dy[dd]
+        if xx<0 or yy<0 or xx>=n or yy>=m:
             continue
 
         if board[xx][yy]==0: 
             dfs(xx, yy, dd)
         else:
-            check+=1 
-    print(check)
+            check+=1
+
     if check==4:
         xx = x+dx[(d+2)%4]
         yy = y+dy[(d+2)%4]
-        print(board)
-        print(" ")
-        if xx<0 or yy<0 or xx>=n or yy>=m or board[xx][yy]==1:
-            print(cnt)
-        else:
-            dfs(xx, yy, d)
-
+        if xx>=0 and yy>=0 and xx<n and yy<m:
+            print(board[xx][yy], xx, yy)
+            if board[xx][yy]!=1:
+                dfs(xx, yy, d)    
+            else:
+                print(cnt)
+                exit(0)
+            
 dfs(x,y,d)
-# for b in board:
-#     print(*b)
